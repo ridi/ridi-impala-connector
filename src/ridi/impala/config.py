912cfd1e628e812150dd11d791f42ae3cee7bb9d
Lee Chanu <chanu.lee@ridi.com>
2018-05-25 19:13:24 +0900

    Instantiate IMPALA_CONNECT_ARGS inside the set() method

--- src/ridi/impala/config.py
+++ src/ridi/impala/config.py
@@ -4,25 +4,17 @@ ConnectArgsType = namedtuple('ConnectArgsType', [
     'host', 'port', 'user', 'password', 'database'
 ])
 
-IMPALA_CONNECT_ARGS = ConnectArgsType(
-    host="localhost",
-    port=21050,
-    user="user",
-    password="password",
-    database="default",
-)
+IMPALA_CONNECT_ARGS = None
 
 
 class ImpalaConfig(object):
     @staticmethod
-    def set(host=None, port=None, auth=None, database=None):
-        if host:
-            IMPALA_CONNECT_ARGS.host = host
-        if port:
-            IMPALA_CONNECT_ARGS.port = port
-        if auth:
-            user, password = auth
-            IMPALA_CONNECT_ARGS.user = user
-            IMPALA_CONNECT_ARGS.password = password
-        if database:
-            IMPALA_CONNECT_ARGS.database = database
+    def set(host=None, port=None, user=None, password=None, database=None):
+        global IMPALA_CONNECT_ARGS
+        IMPALA_CONNECT_ARGS = ConnectArgsType(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database,
+        )._asdict()
